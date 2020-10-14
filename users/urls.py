369fc60a2c369fa99bd4129edc6a8d8d25cee623
Yomin Song <alsthd27@naver.com>
2020-10-15 05:35:40 +0900

    좋아요 누른 유저 표시, 마이페이지, 팔로워 및 팔로잉 리스트

---
+++ users/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+app_name="users"
+urlpatterns = [
+    path('<int:user_id>/follow_toggle/', follow_toggle, name="follow_toggle"),
+    path('<int:user_id>/mypage/', mypage, name="mypage"),
+]
--- users/views.py
+++ users/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from .models import *
+from posts.models import Post
 
-# Create your views here.
+@login_required
+def follow_toggle(request, user_id):
+    user = request.user
+    followed_user = get_object_or_404(User, pk=user_id)
+    is_follower = user.profile in followed_user.profile.followers.all()
+    
+    if is_follower:
+        user.profile.followings.remove(followed_user.profile)
+    else:
+        user.profile.followings.add(followed_user.profile)
+        
+    return redirect('home')
+
+
+def mypage(request, user_id):
+    user = get_object_or_404(User, pk=user_id)
+
+    context = {
+        'posts' : Post.objects.filter(user=user),
+        'followings' : user.profile.followings.all(),
+        'followers' : user.profile.followers.all(),
+    }
+    
+    return render(request, 'users/mypage.html', context)
